b02b477b9c1e97b51d7888bf72f6cb489c24471a
southorange0929 <southorange0929@foxmail.com>
2023-11-22 14:18:52 +0800

    refactor: refactor subcommand to cargo subcmd

--- cli/cargo-ohrs/src/main.rs
+++ cli/cargo-ohrs/src/main.rs
@@ -1,4 +1,6 @@
-use clap::{Parser, Subcommand};
+use std::process::exit;
+
+use clap::Parser;
 
 mod build;
 mod doctor;
@@ -6,33 +8,66 @@ mod init;
 mod tmps;
 
 #[derive(Parser)]
-#[command(author,version,about = "A cli tool for ohos-rs to init,build,doctor & etc.",long_about = None)]
-struct Cli {
-  #[command(subcommand)]
-  pub command: Commands,
+#[command(name = "cargo")]
+#[command(bin_name = "cargo")]
+enum OhosRsCli {
+  #[command(about = "run init command")]
+  OHOS(Ohos),
 }
 
-#[derive(Subcommand)]
-enum Commands {
-  #[command(about = "run init command")]
-  Init { name: String },
-  #[command(about = "run build command")]
-  Build,
-  #[command(about = "run doctor command")]
-  Doctor,
+#[derive(clap::Args)]
+#[command(author, version, about, long_about = None)]
+struct Ohos {
+  /// build target
+  #[arg(short,long)]
+  build: bool,
+
+  /// init project
+  #[arg(short,long)]
+  init: Option<String>,
+
+  /// validate env
+  #[arg(short,long)]
+  doctor: bool,
 }
 
 fn main() {
-  let arg = Cli::parse();
-  match arg.command {
-    Commands::Build => {
-      build::build();
-    }
-    Commands::Init { name } => {
-      init::init(name);
-    }
-    Commands::Doctor => {
-      doctor::doctor();
+  let arg = OhosRsCli::parse();
+  match arg {
+    OhosRsCli::OHOS(args) => {
+      let build_flag = args.build;
+      let init_flag = args.init.is_some();
+      let doctor_flag = args.doctor;
+      let mut count = 0;
+      if build_flag {
+        count = count + 1;
+        print!("log1");
+      }
+      if init_flag {
+        count = count + 1;
+      }
+      if doctor_flag {
+        count = count + 1;
+      }
+      if count == 0 {
+        println!("Must provide one of --build,--init,--doctor");
+        exit(-1);
+      }
+      if count > 1 {
+        println!("Received too many arguments, just provide one of --build,--init,--doctor");
+        exit(-1)
+      }
+      if build_flag {
+        build::build();
+        return;
+      }
+      if let Some(name) = args.init {
+        init::init(name);
+        return;
+      }
+      if doctor_flag {
+        doctor::doctor();
+      }
     }
   }
 }

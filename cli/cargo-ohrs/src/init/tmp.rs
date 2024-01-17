f37a45e0e9fb8665cf790262cab67a2850616ee1
southorange0929 <southorange0929@foxmail.com>
2024-01-17 16:07:05 +0800

    feat: template don't need pre_init logic anymore

--- cli/cargo-ohrs/src/init/tmp.rs
+++ cli/cargo-ohrs/src/init/tmp.rs
@@ -91,17 +91,10 @@ napi-build-ohos = { version = "*" }
 "#;
 
 pub const LIB_CODE: &str = r#"use napi_derive_ohos::napi;
-use napi_ohos::bindgen_prelude::pre_init;
-use napi_ohos::module_init;
 
 #[napi]
 pub fn add(left: u32, right: u32) -> u32 {
   left + right
-}
-
-#[module_init]
-fn init() {
-  pre_init();
 }
 "#;
 

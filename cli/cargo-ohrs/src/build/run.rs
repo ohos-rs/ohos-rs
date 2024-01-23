89d6ae9e595fe8c63ee2737c3bb139bc2215e35c
southorange0929 <southorange0929@foxmail.com>
2024-01-23 18:06:25 +0800

    refactor: refactor build command & remove some script

--- cli/cargo-ohrs/src/build/run.rs
+++ cli/cargo-ohrs/src/build/run.rs
@@ -1,13 +1,43 @@
 use crate::build::{Architecture, Context};
+use convert_case::{Case, Casing};
+use std::collections::HashMap;
+use std::env;
 use std::io::{BufRead, BufReader};
 use std::process::{exit, Command, Stdio};
 
 pub fn build(ctx: &mut Context, arch: &Architecture) {
   let mut args = ctx.init_args.clone();
   args.extend(["--target", &arch.target]);
+  let ndk = env::var("OHOS_NDK_HOME").expect("OHOS_NDK_HOME not set");
+
+  let ohos_c_cflags = HashMap::from([
+        ("arm64", format!("-target aarch64-linux-ohos --sysroot={}/native/sysroot -D__MUSL__", ndk)),
+        ("arm", format!("-target arm-linux-ohos --sysroot={}/native/sysroot -D__MUSL__ -march=armv7-a -mfloat-abi=softfp -mtune=generic-armv7-a -mthumb", ndk)),
+        ("x86_64", format!("-target x86_64-linux-ohos --sysroot={}/native/sysroot -D__MUSL__", ndk)),
+    ]);
+
+  let linker = &format!(
+    "CARGO_TARGET_{}_LINKER",
+    &arch.target.to_case(Case::UpperSnake)
+  );
+
+  let build_env_map = HashMap::from([
+    ("CC", format!("{}/native/llvm/bin/clang", ndk)),
+    ("CXX", format!("{}/native/llvm/bin/clang++", ndk)),
+    ("AR", format!("{}/native/llvm/bin/llvm-ar", ndk)),
+    ("RANLIB", format!("{}/native/llvm/bin/llvm-ranlib", ndk)),
+    (linker, format!("{}/native/llvm/bin/clang", ndk)),
+    (
+      "CFLAGS",
+      ohos_c_cflags.get(arch.platform).unwrap().to_string(),
+    ),
+  ]);
+
+  println!("{:?}",build_env_map);
 
   let mut child = Command::new("cargo")
     .args(args)
+    .envs(build_env_map)
     .stdout(Stdio::piped())
     .spawn()
     .expect("Failed to execute command");

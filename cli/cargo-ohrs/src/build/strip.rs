4fbad4cf45c80716ea7383acfe19b8da3c8ee81c
southorange0929 <southorange0929@foxmail.com>
2024-03-21 17:19:06 +0800

    fix: strip will curse missing symbol with debug mode

--- cli/cargo-ohrs/src/build/strip.rs
+++ cli/cargo-ohrs/src/build/strip.rs
@@ -7,10 +7,8 @@ use std::{
 
 pub fn strip(c: Arc<RwLock<Context>>, threads: &mut Vec<JoinHandle<()>>) {
   let args = super::BUILD_ARGS.read().unwrap();
-  #[allow(unused_assignments)]
-  let mut strip: bool = args.release;
   let ctx = c.read().unwrap();
-  strip = args.strip;
+  let strip = args.strip.unwrap_or(args.release);
   if strip {
     let llvm_strip = Arc::new(format!("{}/native/llvm/bin/llvm-strip", ctx.ndk));
     ctx.dist_files.iter().for_each(|p| {

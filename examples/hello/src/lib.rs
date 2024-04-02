f2113036c7717aaeba9d4e6233b3ad2f2f71a469
southorange0929 <southorange0929@foxmail.com>
2024-04-02 17:31:55 +0800

    feat: support abortcontroller

--- examples/hello/src/lib.rs
+++ examples/hello/src/lib.rs
@@ -1,8 +1,5 @@
 use napi_derive_ohos::napi;
-use napi_ohos::{
-  bindgen_prelude::*,
-  threadsafe_function::{ThreadsafeFunction, ThreadsafeFunctionCallMode, UnknownReturnValue},
-};
+use napi_ohos::{bindgen_prelude::*, JsNumber, threadsafe_function::{ThreadsafeFunction, ThreadsafeFunctionCallMode, UnknownReturnValue}};
 
 #[napi]
 pub fn sum(left: i32, right: i32) -> i32 {
@@ -40,4 +37,34 @@ impl Utils {
   pub fn sum(&self, left: i32, right: i32) -> i32 {
     left + right
   }
+}
+
+fn fib(n: u32) -> u32 {
+  match n {
+    0 => 0,
+    1 => 1,
+    _ => fib(n - 1) + fib(n - 2),
+  }
+}
+
+struct AsyncFib {
+  input: u32,
+}
+
+impl Task for AsyncFib {
+  type Output = u32;
+  type JsValue = JsNumber;
+
+  fn compute(&mut self) -> Result<Self::Output> {
+    Ok(fib(self.input))
+  }
+
+  fn resolve(&mut self, env: Env, output: u32) -> Result<Self::JsValue> {
+    env.create_uint32(output)
+  }
+}
+
+#[napi]
+fn async_fib(input: u32, signal: AbortSignal) -> AsyncTask<AsyncFib> {
+  AsyncTask::with_signal(AsyncFib { input }, signal)
 }

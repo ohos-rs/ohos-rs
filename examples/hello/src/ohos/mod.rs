6deec23c24f758b2399ded2fb8b85283b46df649
southorange0929 <southorange0929@foxmail.com>
2024-04-23 18:11:28 +0800

    chore: update example code

--- examples/hello/src/ohos/mod.rs
+++ examples/hello/src/ohos/mod.rs
@@ -3,19 +3,29 @@ use napi_ohos::{ark::ArkRuntime, Env, Result};
 
 #[napi]
 pub fn load_module_with_default_env(env: Env) -> Result<()> {
-    let runtime = ArkRuntime::new_with_env(env);
-    let module = runtime.load("@ohos.log")?;
-    let js_string = env.create_string("Hello Harmony".as_ref())?;
-    module.call("info", &[js_string])?;
-    Ok(())
+  let runtime = ArkRuntime::new_with_env(env);
+  let module = runtime.load("@ohos.log")?;
+  let js_string = runtime
+    .env
+    .create_string("Hello Harmony".as_ref())?
+    .into_unknown();
+  let tag = runtime.env.create_string("test".as_ref())?.into_unknown();
+  let flag = runtime.env.create_int32(0)?.into_unknown();
+  module.call("info", &[flag, tag, js_string])?;
+  Ok(())
 }
 
 #[napi]
-pub fn load_module_with_new_runtime(env: Env) -> Result<()> {
-    let runtime = ArkRuntime::new()?;
-    let module = runtime.load("@ohos.log")?;
-    // for new runtime, you should use runtime's env to call some method
-    let js_string = runtime.env.create_string("Hello Harmony".as_ref())?;
-    module.call("info", &[js_string])?;
-    Ok(())
+pub fn load_module_with_new_runtime() -> Result<()> {
+  let runtime = ArkRuntime::new()?;
+  let module = runtime.load("@ohos.log")?;
+  // for new runtime, you should use runtime's env to call some method
+  let js_string = runtime
+    .env
+    .create_string("Hello Harmony".as_ref())?
+    .into_unknown();
+  let tag = runtime.env.create_string("test".as_ref())?.into_unknown();
+  let flag = runtime.env.create_int32(0)?.into_unknown();
+  module.call("info", &[flag, tag, js_string])?;
+  Ok(())
 }

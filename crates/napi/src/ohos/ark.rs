a44c6210cd3384867fe95446cbf43c96ec27085b
southorange0929 <southorange0929@foxmail.com>
2024-05-13 09:42:49 +0800

    fix(napi): napi_load_module should check pending exception

--- crates/napi/src/ohos/ark.rs
+++ crates/napi/src/ohos/ark.rs
@@ -1,6 +1,6 @@
 use std::{ffi::CString, ptr};
 
-use crate::{check_status, Env, Result};
+use crate::{check_pending_exception, check_status, Env, Result};
 
 use super::module::Module;
 
@@ -34,30 +34,26 @@ impl ArkRuntime {
   /// note: This method only can call in main thread, the runtime must be initialized with `new_with_env`
   pub fn load<T: AsRef<str>>(&self, path: T) -> Result<Module> {
     let c_path = CString::new(path.as_ref())?;
-    unsafe {
-      let mut module = ptr::null_mut();
-      check_status!(napi_sys_ohos::napi_load_module(
-        self.env.0,
-        c_path.as_ptr(),
-        &mut module
-      ))?;
-      Ok(Module::new(self.env.0, module))
-    }
+    let mut module = ptr::null_mut();
+    check_pending_exception!(self.env.0, unsafe {
+      napi_sys_ohos::napi_load_module(self.env.0, c_path.as_ptr(), &mut module)
+    })?;
+    Ok(Module::new(self.env.0, module))
   }
 
   pub fn load_with_info<T: AsRef<str>>(&self, path: T, module_info: T) -> Result<Module> {
     let c_path = CString::new(path.as_ref())?;
     let c_info = CString::new(module_info.as_ref())?;
-    unsafe {
-      let mut module = ptr::null_mut();
-      check_status!(napi_sys_ohos::napi_load_module_with_info(
+    let mut module = ptr::null_mut();
+    check_pending_exception!(self.env.0, unsafe {
+      napi_sys_ohos::napi_load_module_with_info(
         self.env.0,
         c_path.as_ptr(),
         c_info.as_ptr(),
-        &mut module
-      ))?;
-      Ok(Module::new(self.env.0, module))
-    }
+        &mut module,
+      )
+    })?;
+    Ok(Module::new(self.env.0, module))
   }
 }
 
